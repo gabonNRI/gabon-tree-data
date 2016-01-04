cfb8fc322d6be4d6e46d5af2ad02809f83e166ba
SR Melody <srmelody@gmail.com>
2016-01-03 20:48:54 -0500

    Added json functions

--- R/heights.R
+++ R/heights.R
@@ -3,14 +3,22 @@ heightRegression <- function (df) {
   df$height <- 42
   df
 }
-
+testPayload <- function() {
+  json = '[{"tag": "tag1", "diameter": 30.4, "calculatedHeight": 409}]'
+  receiveHeightsPayload(json)
+}
+receiveHeightsPayload <- function(json) {
+  library(jsonlite)
+  args = fromJSON(json, simplifyDataFrame = true)
+  computeHeights(args)
+}
 computeHeights <-
-  function(input)
+  function(frame)
   {
     
-    trees.str = '"tag1",30.4, 399
- "tag2", 42, 100'
-    frame = read.csv(text=trees.str, header=FALSE)
+ #   trees.str = '"tag1",30.4, 399
+ #"tag2", 42, 100'
+#    frame = read.csv(text=trees.str, header=FALSE)
     colnames(frame) <- c("tag", "diameter", "calculated_height" )
     result = heightRegression(frame)
   }
