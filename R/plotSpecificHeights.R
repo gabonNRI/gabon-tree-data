64e32d5266574e7b8968d6e274a74743f80814eb
Stephen Cole <dirtdog@gmail.com>
2018-08-09 11:58:53 -0400

    test

--- R/plotSpecificHeights.R
+++ R/plotSpecificHeights.R
@@ -1,10 +1,10 @@
-library(BIOMASS)
-library(jsonlite)
 
 computePlotSpecificHeights <- function(measurements) {
   # {data: [ {  "Tag": "3407", "D": 40.8, "MH": 15.4, EH: <result> }], function: <string function>}
 
   # dat <- read.csv("nri-p005-trees.csv", header = T, na.string = "null" )
+  library(BIOMASS)
+  library(jsonlite)
   dat = fromJSON(measurements)
   ## Step 1 - Calculate best height model for each stand
 
